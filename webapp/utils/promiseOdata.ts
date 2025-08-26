41c3278d46ff933e72673191a5c6a30652bfa5a2
Pradipta Biswas <pradiptabiswas980@gmail.com>
2025-08-26 14:48:11 +0000

    "promiseOdata" & "showMessageBox" utils

---
+++ webapp/utils/promiseOdata.ts
@@ -0,0 +1,52 @@
+import Deferred from "sap/base/util/Deferred";
+import ODataModel from "sap/ui/model/odata/v2/ODataModel";
+
+//Types
+type RemoveCallbacks<T> = Omit<Exclude<T, undefined>, "success" | "error">;
+export type ReadParam = RemoveCallbacks<Parameters<typeof ODataModel.prototype.read>[1]>;
+export type RemoveParam = RemoveCallbacks<Parameters<typeof ODataModel.prototype.remove>[1]>;
+export type CreateParam = RemoveCallbacks<Parameters<typeof ODataModel.prototype.create>[2]>;
+export type UpdateParam = RemoveCallbacks<Parameters<typeof ODataModel.prototype.update>[2]>;
+export type ODataParams = ReadParam | RemoveParam | CreateParam | UpdateParam | undefined;
+type ODataMethodReturn = { abort: () => void };
+type PromiseODataReturn<T> = { promise: Promise<T>, abort: ODataMethodReturn["abort"] };
+
+type ODataParamByAction =
+    { action: "read", options?: ReadParam }
+    | { action: "remove", options?: RemoveParam }
+    | { action: "create", payload: object, options?: CreateParam }
+    | { action: "update", payload: object, options?: UpdateParam };
+
+
+export default function promiseOdata<T>(
+    model: ODataModel,
+    path: string,
+    param: ODataParamByAction
+): PromiseODataReturn<T> {
+    const { promise, resolve, reject } = new Deferred<T>();
+    const addResolvers = (options?: ODataParams) => {
+        return { ...options, success: resolve, error: reject };
+    };
+
+    let abort = () => { };
+    switch (param.action) {
+        case "read":
+            abort = (model.read(path, addResolvers(param.options)) as ODataMethodReturn).abort;
+            break;
+        case "remove":
+            abort = (model.remove(path, addResolvers(param.options)) as ODataMethodReturn).abort;
+            break;
+        case "create":
+            abort = (model.create(path, param.payload, addResolvers(param.options)) as ODataMethodReturn).abort;
+            break;
+        case "update":
+            abort = (model.update(path, param.payload, addResolvers(param.options)) as ODataMethodReturn).abort;
+            break;
+        default:
+        // Exhaustiveness check
+        const _exhaustiveCheck: never = param;
+        throw new Error(`Unhandled OData action: ${(_exhaustiveCheck as any).action}`);
+    };
+
+    return { promise, abort };
+};
--- webapp/utils/showMessageBox.ts
+++ webapp/utils/showMessageBox.ts
@@ -1,38 +1,64 @@
 import Deferred from "sap/base/util/Deferred";
 import MessageBox from "sap/m/MessageBox";
 
-//Types
-type MBoxMethod = Exclude<keyof MessageBox, "Action" | "Icon">;
-type OptionsForMBoxMethod<M extends MBoxMethod> = 
-    M extends "alert" ? Parameters<typeof MessageBox.alert>[1]
-    : M extends "confirm" ? Parameters<typeof MessageBox.confirm>[1]
-    : M extends "error" ? Parameters<typeof MessageBox.error>[1]
-    : M extends "information" ? Parameters<typeof MessageBox.information>[1]
-    : M extends "show" ? Parameters<typeof MessageBox.show>[1]
-    : M extends "success" ? Parameters<typeof MessageBox.success>[1]
-    : Parameters<typeof MessageBox.warning>[1]; // for "warning method"
+// Types
+type StripOnClose<T> = Omit<Exclude<T, undefined>, "onClose">;
 
-type Action = typeof MessageBox.Action;
-type MBoxActionType = 
-          | (Action | keyof  Action)
-          | Array<Action | keyof  Action>
-          | string
-          | string[];
+export type AlertParam = StripOnClose<Parameters<typeof MessageBox.alert>[1]>;
+export type ConfirmParam = StripOnClose<Parameters<typeof MessageBox.confirm>[1]>;
+export type ErrorParam = StripOnClose<Parameters<typeof MessageBox.error>[1]>;
+export type InformationParam = StripOnClose<Parameters<typeof MessageBox.information>[1]>;
+export type ShowParam = StripOnClose<Parameters<typeof MessageBox.show>[1]>;
+export type SuccessParam = StripOnClose<Parameters<typeof MessageBox.success>[1]>;
+export type WarningParam = StripOnClose<Parameters<typeof MessageBox.warning>[1]>;
 
+type MBParams = AlertParam | ConfirmParam | ErrorParam | InformationParam | ShowParam | SuccessParam | WarningParam;
+type MessageBoxActions = (keyof typeof MessageBox.Action) | (string & {}); // loose suggestion
 
-export default function showMessageBox<M extends MBoxMethod>(
-    action: M,
-    message: string,
-    options?: OptionsForMBoxMethod<M>
-){
-    const {promise, resolve} = new Deferred<MBoxActionType>();
-    const wrappedOptions = {
-        ...options,
-        onClose:(action: MBoxActionType)=>{
-            options?.onClose?.(action);
-            resolve(action)
-        }
-    }
-    MessageBox[action](message, wrappedOptions);
-    return promise
-}
+type MBParamsByAction =
+  { action: "alert", message: string, options?: AlertParam }
+  | { action: "confirm", message: string, options?: ConfirmParam }
+  | { action: "error", message: string, options?: ErrorParam }
+  | { action: "information", message: string, options?: InformationParam }
+  | { action: "show", message: string, options?: ShowParam }
+  | { action: "success", message: string, options?: SuccessParam }
+  | { action: "warning", message: string, options?: WarningParam };
+
+export default function showMessageBox(params: MBParamsByAction): Promise<MessageBoxActions> {
+  const { promise, resolve } = new Deferred<MessageBoxActions>();
+  const addResolvers = (options?: MBParams) => {
+    return { ...options, onClose: resolve };
+  }
+
+  const { action, message, options } = params;
+  switch (action) {
+    case "alert":
+      MessageBox.alert(message, addResolvers(options));
+      break;
+    case "confirm":
+      MessageBox.confirm(message, addResolvers(options));
+      break;
+    case "error":
+      MessageBox.error(message, addResolvers(options));
+      break;
+    case "information":
+      MessageBox.information(message, addResolvers(options));
+      break;
+    case "show":
+      MessageBox.show(message, addResolvers(options));
+      break;
+    case "success":
+      MessageBox.success(message, addResolvers(options));
+      break;
+    case "warning":
+      MessageBox.warning(message, addResolvers(options));
+      break;
+    default:
+      // Exhaustiveness check
+      const _exhaustiveCheck: never = params;
+      throw new Error(`Unhandled MessageBox action: ${(_exhaustiveCheck as any).action}`);
+  };
+
+  return promise;
+};
+
